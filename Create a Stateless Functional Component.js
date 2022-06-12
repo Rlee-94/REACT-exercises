09780e8c68eab4d8d4d85de1e2cde7a2fcc55a75
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-06-12 12:14:17 -0500

    Adding new exercises

---
+++ Create a Stateless Functional Component.js
@@ -0,0 +1,10 @@
+//The code editor has a function called MyComponent. Complete this function so it returns a single div element which contains some string of text.
+
+const MyComponent = function () {
+    // Change code below this line
+    return (
+        <div className='testClass'>Learning REACT and I'm excited!</div>
+    );
+
+    // Change code above this line
+}
