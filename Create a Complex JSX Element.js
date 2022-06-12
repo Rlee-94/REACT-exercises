e99c66cb36df47b3e173f07bc373a4b9117b484f
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-06-12 11:58:37 -0500

    Adding exercise files

---
+++ Create a Complex JSX Element.js
@@ -0,0 +1,12 @@
+//Define a new constant JSX that renders a div which contains the following elements in order:
+//An h1, a p, and an unordered list that contains three li items. You can include any text you want within each element.
+
+const JSX = <div>
+    <h1>Hello World!</h1>
+    <p>I'm learning react</p>
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+</div>
